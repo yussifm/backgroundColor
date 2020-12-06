34824127adae25577c3cbc8f168ecb2b91216e06
yussif mohammed <43506617+yussifm@users.noreply.github.com>
2020-12-06 02:30:34 +0000

    bg-changer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'backgroundColor';
+  color = `rgb(248, 248, 248)`;
+ public red: number;
+ public green: number;
+ public blue: number;
+
+  generator() {
+    this.red = Math.floor(Math.random() * 255);
+    this.green = Math.floor(Math.random() * 255);
+    this.blue = Math.floor(Math.random() * 255);
+
+  }
+
+
+  changer() {
+    this.generator();
+    return this.color = `rgb(${this.red},${this.green},${this.blue})`;
+
+
+  }
+
 }
